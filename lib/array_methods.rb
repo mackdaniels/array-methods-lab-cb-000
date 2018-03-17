19c453232e38489f684656b7c1ccb55b95e69dc2
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-17 16:23:27 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,1 +1,24 @@
+def using_include(array, element)
+  array[element].include?
 
+end
+
+def using_sort(array)
+
+end
+
+def using_reverse(array)
+
+end
+
+def using_first(array)
+
+end
+
+def using_last(array)
+
+end
+
+def using_size(array)
+
+end
