d4f17f8ac648a4d217a69deb6a7be73287771f43
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-17 16:20:37 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,35 @@
-def using_include(array, element)
-
+def create_an_empty_array
+  arr = []
 end
 
-def using_sort(array)
-
+def create_an_array
+[1,2,3,4]
 end
 
-def using_reverse(array)
-
+def add_element_to_end_of_array(array, element)
+  array.push(element)
 end
 
-def using_first(array)
-
+def add_element_to_start_of_array(array, element)
+  array.insert(0, element)
 end
 
-def using_last(array)
-
+def remove_element_from_end_of_array(array)
+  array.pop
 end
 
-def using_size(array)
+def remove_element_from_start_of_array(array)
+ array.shift
+end
 
+def retrieve_element_from_index(array, index_number)
+  array[index_number]
+end
+
+def retrieve_first_element_from_array(array)
+  array[0]
+end
+
+def retrieve_last_element_from_array(array)
+ array[-1]
 end
